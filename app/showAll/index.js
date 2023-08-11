34e03f4bfd654c640de8db05dff5b5920defea43
Alex Mendoza <alex.mendoza.et@gmail.com>
2023-08-11 11:48:15 -0500

    Job-App

---
+++ app/showAll/index.js
@@ -0,0 +1,17 @@
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  FlatList,
+  ActivityIndicator,
+} from "react-native";
+
+const showAll = () => {
+  return (
+    <View>
+      <Text>showAll</Text>
+    </View>
+  );
+};
+
+export default showAll;
